179fb0f989cebcc3393c21fca11b59517227b862
min9kwak <mik414@pitt.edu>
2025-05-11 20:08:41 -0400

    dataset

--- datasets/brats.py
+++ datasets/brats.py
@@ -8,148 +8,85 @@ import numpy as np
 
 class BraTSProcessor(object):
     def __init__(self,
-                 data_root: str = 'D:/data/BraTS/slice',
-                 modality: str = 't1',
+                 data_root: str = 'D:/data/tumor-controller',
+                 modality: str = 't1ce',
                  validation_size: float = 0.1,
                  test_size: float = 0.1,
                  random_state: int = 2023):
 
-        assert modality in ['t1', 't1ce', 't2', 'flair', 'all', 't1ce+t2']
-        self.data_root = data_root
-        self.modality = modality
+        assert modality in ['t1ce', 't2', 't1ce+t2']
+
+        self.tumor_dir = os.path.join(data_root, 'normal')
+        self.healthy_dir = os.path.join()
+
+        self.modality: list = modality.split(modality)
         self.validation_size = validation_size
         self.test_size = test_size
         self.random_state = random_state
 
     def process(self):
+        """
+        Process the tumor and healthy directories to create datasets.
+        Returns two datasets: tumor_set and healthy_set.
+        """
+        tumor_set = self.create_dataset(self.tumor_dir)
+        healthy_set = self.create_dataset(self.healthy_dir)
 
-        patient_ids = os.listdir(self.data_root)
-        train_size = 1 - (self.validation_size + self.test_size)
+        return dict(tumor=tumor_set, healthy=healthy_set)
 
-        train_ids, test_ids = train_test_split(patient_ids, test_size=self.test_size, random_state=self.random_state)
-        train_ids, validation_ids = train_test_split(
-            train_ids, test_size=self.validation_size / (train_size + self.validation_size),
-            random_state=self.random_state
-        )
-
-        train_set = self.create_dataset(train_ids)
-        validation_set = self.create_dataset(validation_ids)
-        test_set = self.create_dataset(test_ids)
-
-        return dict(train=train_set, validation=validation_set, test=test_set)
-
-    def create_dataset(self, ids):
-
+    def create_dataset(self, data_dir):
+        """
+        Create a dataset from the given directory (tumor or healthy).
+        Each entry includes image paths for the specified modalities and the corresponding segmentation path.
+        """
         dataset = []
 
-        for id in ids:
+        for filename in os.listdir(data_dir):
+            # Extract patient ID and modality from the filename
+            patient_id, modality = filename.split('-')[0], filename.split('-')[-1].split('.')[0]
 
-            filenames = os.listdir(os.path.join(self.data_root, id))
-            image_filenames = sorted([f for f in filenames if f'{self.modality}_' in f])
-            seg_filenames = sorted([f for f in filenames if f'seg_' in f])
+            # Process only if the modality matches
+            if modality in self.modality:
+                seg_filename = f"{patient_id}-{filename.split('-')[1]}-seg.pkl"
+                seg_path = os.path.join(data_dir, seg_filename)
 
-            # check slice sequences and pairing
-            for image_filename, seg_filename in zip(image_filenames, seg_filenames):
-                assert image_filename.split('_')[1] == seg_filename.split('_')[1]
-                out = dict(image=os.path.join(self.data_root, id, image_filename),
-                           label=os.path.join(self.data_root, id, seg_filename),
-                           number=f"{id}-{image_filename.replace('.pkl', '')}")
-                dataset.append(out)
+                # Ensure the segmentation file exists
+                if os.path.exists(seg_path):
+                    dataset.append({
+                        "image": os.path.join(data_dir, filename),
+                        "seg": seg_path,
+                        "number": f"{patient_id}-{filename.replace('.pkl', '')}"
+                    })
 
         return dataset
 
 
 class BraTSDataset(Dataset):
     # TODO: remove test_flag
-    def __init__(self, dataset, transform, masking: str = None,
-                 patch_size: int = 4, patch_ratio: float = 0.3):
+    def __init__(self, dataset, image_transform, seg_transform, label):
 
         self.dataset = dataset
-        self.transform = transform
-
-        self.masking = masking
-        self.patch_size = patch_size if masking is not None else None
-        self.patch_ratio = patch_ratio if masking is not None else None
+        self.image_transform = image_transform
+        self.seg_transform = seg_transform
+        self.label = label
 
     def __getitem__(self, idx):
 
         with open(self.dataset[idx]['image'], 'rb') as fb:
             image = pickle.load(fb)
-        with open(self.dataset[idx]['label'], 'rb') as fb:
-            label = pickle.load(fb)
+        with open(self.dataset[idx]['seg'], 'rb') as fb:
+            seg = pickle.load(fb)
 
-        # TODO: transform - at least rotate90
+        if self.image_transform:
+            image = self.image_transform(image)
+        if self.seg_transform:
+            seg = self.seg_transform(seg)
 
-        weak_label = 1 if label.max() > 0 else 0
+        out = dict(x=image, seg=seg, y=self.label, idx=idx)
 
-        out_dict = {}
-        out_dict['y'] = weak_label
-
-        number = self.dataset[idx]['number']
-
-        image = image / image.max()
-
-        # return data sample
-        out = dict(x=image, y=weak_label, out_dict=out_dict, seg=label, idx=number)
-        if self.masking is None:
-            return out
-        elif self.masking == 'half':
-            mask = self.mask_half(label)
-            out['mask'] = mask
-        elif self.masking == 'random_patch':
-            mask = self.mask_random_patch(image)
-            out['mask'] = mask
-        else:
-            raise ValueError('masking must be either "half" or "random_patch"')
         return out
 
-    def mask_random_patch(self, image):
-        #
-        C, H, W = image.shape
-        assert H % self.patch_size == 0 and W % self.patch_size == 0
-        num_patches = (H // self.patch_size) * (W // self.patch_size)
-        num_zero_patches = int(num_patches * self.patch_ratio)
-
-        mask = np.ones_like(image, dtype=image.dtype)
-        indices = np.random.permutation(num_patches)[:num_zero_patches]
-        for idx in indices:
-            row = idx // (W // self.patch_size)
-            col = idx % (W // self.patch_size)
-            mask[:, row * self.patch_size:(row + 1) * self.patch_size,
-                 col * self.patch_size:(col + 1) * self.patch_size] = 0.0
-
-        return mask
-
-    @staticmethod
-    def mask_half(label: np.ndarray):
-        H = label.shape[2]
-        mid_line = H // 2
-
-        tumor_count = np.array(label > 0, dtype=float)
-        upper = tumor_count[..., :mid_line, :].sum()
-        lower = tumor_count[..., mid_line:, :].sum()
-
-        # tumorous area. 0: upper, 1: lower
-        if upper == 0 and lower == 0:
-            flag = np.random.randint(0, 2)
-        elif upper >= lower:
-            flag = 0
-        else:
-            flag = 1
-
-        # create half mask (tumorous). 1 indicates tumorous area
-        mask = np.zeros_like(label)
-        if flag == 0:
-            mask[..., :mid_line, :] = 1.0
-        else:
-            mask[..., mid_line:, :] = 1.0
-
-        return mask
-
-    def __len__(self):
-        return len(self.dataset)
-
-
+    
 if __name__ == '__main__':
 
     processor = BraTSProcessor()
--- datasets/transforms.py
+++ datasets/transforms.py
@@ -1,84 +1,20 @@
 import random
 import torch
-from monai.transforms import (
-    Transform, ToTensor, ScaleIntensity, SpatialPad, CenterSpatialCrop, RandRotate90, RandFlip, Compose
-)
+from monai.transforms import Compose, EnsureChannelFirst, ToTensor, Resize
 from monai.utils.enums import TransformBackends
 
 
-class AddChannel(Transform):
-    backend = [TransformBackends.TORCH]
+def make_transforms(image_size: int = 512):
+    
+    image_transform = Compose([
+        EnsureChannelFirst(),
+        ToTensor(torch.float32),
+        Resize(spatial_size=(image_size, image_size), mode='bilinear')
+    ])
+    seg_transform = Compose([
+        EnsureChannelFirst(),
+        ToTensor(torch.long),
+        Resize(spatial_size=(image_size, image_size), mode='bilinear')
+    ])
 
-    def __init__(self):
-        pass
-
-    def __call__(self, img: torch.Tensor):
-        return img.unsqueeze(dim=0)
-
-
-class RandomSlice(Transform):
-    backend = [TransformBackends.TORCH]
-
-    def __init__(self, orientations, image_size: int = 128, slice_range: float = 0.15):
-        assert all(orientation in [0, 1, 2] for orientation in orientations), "Orientations must be in [0, 1, 2]"
-
-        center = image_size // 2
-        interval = int(image_size * slice_range)
-        self.orientations = orientations
-        self.slice_range = (center - interval, center + interval + 1)
-
-    def __call__(self, img: torch.Tensor):
-        dim = random.choice(self.orientations)
-        point = random.choice(range(*self.slice_range))
-
-        slice_indices = [slice(None)] * 4
-        slice_indices[dim + 1] = point
-        out = img[slice_indices]
-        return out
-
-
-class CenterSlice(Transform):
-    backend = [TransformBackends.TORCH]
-
-    def __init__(self, orientations: int, image_size: int = 128):
-        self.center = image_size // 2
-        assert orientations in [0, 1, 2]
-        self.orientations = orientations
-
-    def __call__(self, img: torch.Tensor):
-        slice_indices = [slice(None)] * 4
-        slice_indices[self.orientations + 1] = self.center
-        out = img[slice_indices]
-        return out
-
-
-def make_transforms(image_size: int = 128,
-                    orientations: tuple = (0, 1, 2),
-                    slice_range: float = 0.15,
-                    rotate: bool = False,
-                    flip: bool = False,
-                    prob: float = 0.5):
-
-    base_transform = [ToTensor(),
-                      ScaleIntensity(),
-                      AddChannel(),
-                      SpatialPad(spatial_size=(145, 145, 145)),
-                      CenterSpatialCrop(roi_size=(image_size, image_size, image_size)),
-                      RandomSlice(orientations=orientations, image_size=image_size, slice_range=slice_range)]
-
-    train_transform, test_transform = base_transform.copy(), base_transform.copy()
-
-    if rotate:
-        train_transform.append(RandRotate90(prob=prob))
-    if flip:
-        train_transform.append(RandFlip(prob=prob))
-
-    return Compose(train_transform), Compose(test_transform)
-
-
-def make_center_transforms(image_size: int = 128, orientations: int = 0):
-
-    center_transform = [ToTensor(), ScaleIntensity(), AddChannel(), SpatialPad(spatial_size=(145, 145, 145)),
-                        CenterSpatialCrop(roi_size=(image_size, image_size, image_size)),
-                        CenterSlice(orientations=orientations, image_size=image_size)]
-    return Compose(center_transform)
+    return image_transform, seg_transform
