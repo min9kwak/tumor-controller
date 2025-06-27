026c5f635cfcdb81d0a180c6a6a3d3c92a1040fe
min9kwak <mik414@pitt.edu>
2025-06-27 14:52:40 -0400

    use dict and arparse

--- utils/util.py
+++ utils/util.py
@@ -1,19 +1,35 @@
-def set_env(config: dict, server: str = None) -> dict:
+import argparse
+from typing import Union
+
+def set_env(config: Union[dict, argparse.Namespace], server: str = None) -> Union[dict, argparse.Namespace]:
+    # set get and set for dict and argparse.Namespace
+    def get(key, default=None):
+        if isinstance(config, dict):
+            return config.get(key, default)
+        else:
+            return getattr(config, key, default)
+
+    def set(key, value):
+        if isinstance(config, dict):
+            config[key] = value
+        else:
+            setattr(config, key, value)
+
     # set server
     if server is not None:
-        config['server'] = server
-    elif 'server' not in config:
+        set('server', server)
+    elif get('server') is None:
         raise ValueError("server must be provided either as a parameter or in config")    
-    server = config['server']
-    
+    server = get('server')
+
     assert server in ['local', 'psc']
-    
+
     # set data root
     if server == 'local':
-        config['data_root'] = 'D:/data/tumor-controller'
+        set('data_root', 'D:/data/tumor-controller')
+        set('cache_dir', 'D:/data/tumor-controller/models')
     elif server == 'psc':
-        config['data_root'] = '/ocean/projects/med230010p/mkwak/data/tumor-controller'
-    else:
-        raise ValueError(f"Invalid server: {server}")
+        set('data_root', '/ocean/projects/med230010p/mkwak/data/tumor-controller')
+        set('cache_dir', '/ocean/projects/med230010p/mkwak/models')
     
     return config
